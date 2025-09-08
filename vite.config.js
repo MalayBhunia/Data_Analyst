f5ccc6088944f040ce6e5ea42d1db282592cfb13
Malay Bhunia <mbhunia098@gmail.com>
2025-09-08 18:55:38 +0530

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -3,7 +3,7 @@ import react from '@vitejs/plugin-react'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-    base: '/My-React-Portfolio/',
+    base: '/ppppp/',
     plugins: [react()],
     build: {
         rollupOptions: {
