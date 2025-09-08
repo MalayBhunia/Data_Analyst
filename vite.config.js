cbabf56d884440bf829a967be935e968e9585467
Malay Bhunia <mbhunia098@gmail.com>
2025-09-08 20:48:36 +0530

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -3,7 +3,7 @@ import react from '@vitejs/plugin-react'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-    base: '/ppppp/',
+    base: '/Data_Analyst/',
     plugins: [react()],
     build: {
         rollupOptions: {
